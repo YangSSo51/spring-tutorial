8c8128ae32bcaece5eedcb61a929581c6122cd2f
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2024-07-09 15:37:35 +0900

    Fix: Mysql 연결 정보에 맞게 수정

--- src/main/java/com/tutorial/spring/global/config/SecurityConfig.java
+++ src/main/java/com/tutorial/spring/global/config/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig {
     private static final String[] ALLOW_PATTERNS = {
             "/",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/users/**"
     };
 
     @Bean
